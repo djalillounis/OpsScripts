2f2673a96a26589a322dbba311de612f3877e55c
djalillounis <djalillounis3@gmail.com>
2020-06-11 20:23:19 +0200

    Adding the currentSlot function

--- SendTransaction.py
+++ SendTransaction.py
@@ -19,15 +19,37 @@ def sendCmdtoShell(cmd):
     return res
 
 #local currentTip=$(${cardanocli} shelley query tip ${magicparam} | awk 'match($0,/unSlotNo = [0-9]+/) {print substr($0, RSTART+11,RLENGTH-11)}')
-def calculateFee():
+def currentSlot():
     cmd = "cardano-cli shelley  query tip "+Network
     res=sendCmdtoShell(cmd)
+    #res= "Tip (SlotNo {unSlotNo = 579641}) (ShelleyHash {unShelleyHash = HashHeader {unHashHeader = 4c18abf6f6916872f1128575c2be88b5daac6c946b5fce26b883646164f0716a}}) (BlockNo {unBlockNo = 26720})"
     res = str(res)
     regex = r"\{unSlotNo = [0-9]+}"
+    reg= re.compile(regex)
 
-    matches1 = re.finditer(regex, res, re.MULTILINE)
+    CurrentSlot = reg.findall(res)
+    if CurrentSlot:
+       CurrentSlot = CurrentSlot[0]
+       CurrentSlot =CurrentSlot.replace('}','')
+       CurrentSlot =CurrentSlot.replace('{','')
+       CurrentSlot =CurrentSlot.split("=")
+       if CurrentSlot:
+         CurrentSlot=CurrentSlot[1]
+         CurrentSlot =CurrentSlot.replace(' ','')
+         CurrentSlot =CurrentSlot.replace(' ','')
 
-    return matches1
 
-test=(calculateFee())
-print(test)
+
+
+    else:
+       CurrentSlot=0000
+
+
+
+
+
+    return CurrentSlot
+
+test=(currentSlot())
+
+print (test)
