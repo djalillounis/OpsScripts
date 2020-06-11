a84daf56cbf6559f47cd0414daa4118ee905586e
djalillounis <djalillounis3@gmail.com>
2020-06-11 21:26:29 +0200

    Adding The Calculate Parm Function

--- SendTransaction.py
+++ SendTransaction.py
@@ -2,7 +2,9 @@
 
 import os
 import re
-
+import subprocess
+import time
+import datetime
 
 #---------------Global Variables-----------------
 
@@ -15,15 +17,30 @@ Network = "--testnet-magic 42"
 
 
 def sendCmdtoShell(cmd):
-    res = os.system(cmd)
+    result = subprocess.run([cmd], stdout=subprocess.PIPE,shell=True)
+    res = result.stdout
+    res = str(res)
     return res
 
-#local currentTip=$(${cardanocli} shelley query tip ${magicparam} | awk 'match($0,/unSlotNo = [0-9]+/) {print substr($0, RSTART+11,RLENGTH-11)}')
+
+def protocolParms(Network):
+
+    datetime.datetime.now().strftime('%y-%m-%d %a %H:%M:%S')
+    fileName = datetime.datetime.now().strftime('%y-%m-%d %a %H:%M:%S')
+    fileName = str(fileName)
+    fileName = fileName+".json"
+
+    outFile = "--out-file " + fileName
+    cmd="cardano-cli shelley query protocol-parameters"+Network+outFile
+    sendCmdtoShell(cmd)
+    Print ("Parm  File Created : " + )
+
+
 def currentSlot():
     cmd = "cardano-cli shelley  query tip "+Network
     res=sendCmdtoShell(cmd)
-    #res= "Tip (SlotNo {unSlotNo = 579641}) (ShelleyHash {unShelleyHash = HashHeader {unHashHeader = 4c18abf6f6916872f1128575c2be88b5daac6c946b5fce26b883646164f0716a}}) (BlockNo {unBlockNo = 26720})"
     res = str(res)
+    print(res)
     regex = r"\{unSlotNo = [0-9]+}"
     reg= re.compile(regex)
 
@@ -38,19 +55,30 @@ def currentSlot():
          CurrentSlot=CurrentSlot[1]
          CurrentSlot =CurrentSlot.replace(' ','')
          CurrentSlot =CurrentSlot.replace(' ','')
-
-
-
-
     else:
        CurrentSlot=0000
-
-
-
-
-
     return CurrentSlot
+#---------------Calculate Fee---------------------------------
+def calcultaFee(type,CurrentSlot,Network,signingKeyFile):
 
-test=(currentSlot())
+    txOutCount =0
+    if type="Self":
+        txOutCount =1
+    else:
+        txOutCount =2
+    txInCount = 1
+    tx-in-count = "--tx-in-count "+txInCount
+    tx-out-count = "--tx-out-count "+txOutCount
+    ttl = "--ttl "+CurrentSlot
+    igning-key-file ="--signing-key-file "+signingKeyFile
 
-print (test)
+
+
+
+    cmd = "cardano-cli shelley transaction calculate-min-fee"
+
+
+
+
+
+protocolParms(Network)
